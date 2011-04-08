6219075dd71cf329075df117fe1a392c918ae9ea
David Sansome <me@davidsansome.com>
2011-04-08 20:54:16 +0100

    Do things with hash parts of URLs.

--- static/index.js
+++ static/index.js
@@ -7,6 +7,8 @@ var SearchController = Class.create({
     this.content_element = content_element;
     this.header_element = header_element;
 
+    this.ignore_next_hash_change = false;
+
     // Listen for key events on the input box.
     this.input_element.observe('keyup', this.input_changed.bind(this));
 
@@ -17,16 +19,22 @@ var SearchController = Class.create({
     this.content_element.observe('load', this.content_frame_changed.bind(this));
     this.content_frame_changed();
 
-    // Fill the results list initially and focus the search box.
-    this.search("");
+    // Browse to the page the user put in the hash part of the URL.
+    if (window.location.hash) {
+      this.browse_to(window.location.hash);
+      this.activate_selection();
+    } else {
+      this.search("");
+    }
+
     this.input_element.focus();
   },
 
   input_changed: function(event) {
-    this.search(this.input_element.value);
+    this.search(this.input_element.value, true);
   },
 
-  search: function(search_text) {
+  search: function(search_text, highlight) {
     // Don't do anything if the text wasn't changed since last time.
     if (search_text == this.search_text)
       return;
@@ -89,7 +97,7 @@ var SearchController = Class.create({
 
       // Highlight the search terms in the result
       var highlighted_name = result.name;
-      if (tokens.length != 0) {
+      if (highlight && tokens.length != 0) {
         highlighted_name = highlighted_name.replace(
           highlight_regexp, '<span class="highlight">$1</span>');
       }
@@ -124,6 +132,9 @@ var SearchController = Class.create({
   },
 
   key_pressed: function(event) {
+    if (event.altKey || event.ctrlKey)
+      return;
+
     var element = Event.findElement(event);
 
     switch (event.keyCode) {
@@ -146,7 +157,14 @@ var SearchController = Class.create({
 
       default:
         if (event.keyCode >= 65 && event.keyCode <= 90 && element != this.input_element) {
-          this.input_element.value += String.fromCharCode(event.keyCode).toLowerCase();
+          var char = String.fromCharCode(event.keyCode);
+          if (!event.shiftKey)
+            char = char.toLowerCase();
+
+          // Stop the event before focusing the input box or it'll be added
+          // twice.
+          Event.stop(event);
+          this.input_element.value += char;
           this.input_element.focus();
         }
         return;
@@ -208,19 +226,76 @@ var SearchController = Class.create({
   },
 
   activate_li: function(li) {
+    this.ignore_next_hash_change = true;
     this.content_element.src = li.select("a")[0].href;
   },
 
   content_frame_changed: function(event) {
-    var frame_document = this.content_element.contentWindow.document;
+    var frame_window = this.content_element.contentWindow;
+    var frame_document = frame_window.document;
 
     // Update the title
     var title = frame_document.title;
     this.header_element.update(title.escapeHTML());
     document.title = title;
 
-    // Listen for keypresses
+    // Listen for keypresses on the new frame
     Event.observe(frame_document, 'keydown', this.key_pressed.bind(this));
+
+    // Listen for hash changes
+    Event.observe(frame_window, 'hashchange', this.content_hash_changed.bind(this));
+    this.content_hash_changed();
+  },
+
+  content_hash_changed: function(event) {
+    var name = this.content_element.contentWindow.location.hash;
+    if (!name)
+      return;
+
+    // Take any module- prefix off module names.
+    if (name.startsWith("#module-"))
+      name = "#" + name.substring(8);
+
+    window.history.replaceState(name, "", name);
+
+    if (this.ignore_next_hash_change) {
+      this.ignore_next_hash_change = false;
+      return;
+    }
+
+    this.browse_to(name);
+  },
+
+  browse_to: function(name) {
+    // Take the # off
+    name = name.substring(1);
+  
+    // Find the top-level symbol name.
+    var top_level_symbol = name;
+    if (top_level_symbol.indexOf(".") != -1)
+      top_level_symbol = top_level_symbol.substring(0, top_level_symbol.indexOf("."));
+
+    // Try to find a documentation entry with this name
+    var len = data.length;
+    for (var i=0 ; i<len ; ++i) {
+      if (data[i][1] != name)
+        continue;
+
+      // Got one - do a search for the top-level symbol
+      this.search(top_level_symbol, false);
+
+      // Now select the right search result
+      var results = this.results_element.select("li");
+      var results_len = results.length;
+      for (var j=0 ; j<results_len ; ++j) {
+        if (results[j].down().innerText == name) {
+          this.set_selection(j);
+          break;
+        }
+      }
+
+      break;
+    }
   }
 });
 
